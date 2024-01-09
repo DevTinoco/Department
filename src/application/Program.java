5a8b7028a4b07bf1bd9bbe7731f366d35899b91b
Valdinei Tinoco <valdinei_ct@hotmail.com>
2024-01-08 23:18:33 -0300

    Iniciando o programa

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,36 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 
+		//PROGRAMA PRINCIPAL:
 		
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+	
+		System.out.print("Enter department's name: ");
+		String departmentName = sc.nextLine();
+		System.out.print("Enter worker data: ");
+		System.out.print("Name: ");
+		String workerName = sc.nextLine();
+		System.out.print("Level: ");
+		String workerLevel= sc.nextLine();
+		System.out.print("Base salary: ");
+		double baseSalary = sc.nextDouble();
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
 		
 	}
 
